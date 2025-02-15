cb39f74c1d0b4d5c213a18efc945122630c6b5ea
kanna114 <yjkohi114@gmail.com>
2025-02-15 12:13:06 +0900

    update

--- mix.py
+++ mix.py
@@ -20,23 +20,14 @@ def read_json(file_path: Path):
 
 
 
-def getMixEmoji(text, fn):
-    emoji_code_1 = hex(ord(text[0]))[2:]
-    if(len(text) >= 2):
-        emoji_code_2 = hex(ord(text[1]))[2:]
-        if(emoji_code_2 == "fe0f" and len(text) >= 3):
-            emoji_code_2 = hex(ord(text[2]))[2:]
-        if(emoji_code_2 == "fe0f" and len(text) == 2):
-            emoji_code_2 = "1f422"
-    else:
-        emoji_code_2 = "1f422"
+def getMixEmoji(emoji1, emoji2, fn):
+
+    emoji_code_1 = "-".join(f"{hex(ord(c))[2:]}" for c in emoji1)
+    emoji_code_2 = "-".join(f"{hex(ord(c))[2:]}" for c in emoji2)
 
     info = read_json(MIX_DIR / "metadata4.json")
 
-    if(emoji_code_1 + "-fe0f" in info):
-        emoji_code_1 = emoji_code_1 + "-fe0f"
-    if(emoji_code_2 + "-fe0f" in info):
-        emoji_code_2 = emoji_code_2 + "-fe0f"
+
     if(emoji_code_1 not in info or emoji_code_2 not in info ):
         return -1
     
@@ -59,18 +50,21 @@ def getMixEmoji(text, fn):
 @mix.handle()
 async def handle_first_receive(matcher: Matcher, event: Event, msg: Message = CommandArg()):
     args = str(msg).split(" ")
-    if(len(args) == 1):
+    if(len(args) == 2):
         mix_out = MIX_DIR / "mix.png"
-        e = getMixEmoji(args[0], str(mix_out))
-        await mix.finish(MessageSegment.image(mix_out.resolve().as_uri()))
-    if(len(args) >= 2):
+        e = getMixEmoji(args[0], args[1], str(mix_out))
+        if(e == 0):
+            await mix.finish(MessageSegment.image(mix_out.resolve().as_uri()))
+        else:
+            await mix.finish("可能为不存在的表情组合")
+    if(len(args) >= 4):
         power_factor = 1
-        if(len(args) > 2):
-            power_factor = float(args[2])
-        stats = getMixEmoji(args[0], str(MIX_DIR / "img1.png"))
+        if(len(args) > 4):
+            power_factor = float(args[4])
+        stats = getMixEmoji(args[0], args[1], str(MIX_DIR / "img1.png"))
         if(stats < 0):
             await mix.finish("指令中可能有不存在的表情组合")
-        stats = getMixEmoji(args[1], str(MIX_DIR / "img2.png"))
+        stats = getMixEmoji(args[2], args[3], str(MIX_DIR / "img2.png"))
         if(stats < 0):
             await mix.finish("指令中可能有不存在的表情组合")
 
@@ -90,6 +84,6 @@ async def handle_first_receive(matcher: Matcher, event: Event, msg: Message = Co
             
         gif_out = MIX_DIR / "out.gif"
         gif_frames[0].save(str(gif_out),
-                save_all=True, append_images=gif_frames[1:], optimize=False, duration=20, loop=0, transparency=0)
+                save_all=True, append_images=gif_frames[1:], optimize=False, duration=15, loop=0, transparency=0)
         
         await mix.finish(MessageSegment.image(gif_out.resolve().as_uri()))
